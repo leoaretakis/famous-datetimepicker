941a7a1bb71dc1c8751d6789e473172b5fbd4179
Leonardo Aretakis <leoaretakis@gmail.com>
2014-09-10 01:43:00 +0100

    base app view

---
+++ src/app/AppView.js
@@ -0,0 +1,29 @@
+define(function(require, exports, module) {
+    // var Surface         = require('famous/core/Surface');
+    var View            = require('famous/core/View');
+    var StateModifier = require('famous/modifiers/StateModifier');
+
+    var PageView = require('./PageView')
+
+    function AppView() {
+        View.apply(this, arguments);
+
+        _createPageView.call(this);
+    }
+
+    AppView.prototype = Object.create(View.prototype);
+    AppView.prototype.constructor = AppView;
+
+    AppView.DEFAULT_OPTIONS = {
+    };
+
+    function _createPageView(){
+        this.pageView = new PageView();
+        this.pageModifier = new StateModifier();
+
+        this.add(this.pageModifier).add(this.pageView);
+    }
+
+    module.exports = AppView;
+
+});
