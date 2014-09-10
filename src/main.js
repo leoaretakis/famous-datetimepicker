b3cbccb6aeed0854d7845526af8d85423f49d13d
Leonardo Aretakis <leoaretakis@gmail.com>
2014-09-10 15:24:57 +0100

    main refactoring

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 define(function(require, exports, module) {
     var Engine          = require('famous/core/Engine');
-    var AppView         = require('app/AppView');
     require('famous/inputs/FastClick');
 
-    var mainContext = Engine.createContext();
+    var AppModel         = require('app/models/App');
+    var AppView         = require('app/views/AppView');
 
-    var appView = new AppView();
+    var model = new AppModel();
+    var appView = new AppView(model);
+
+    var mainContext = Engine.createContext();
+    mainContext.setPerspective(1000);
 
     mainContext.add(appView);
 });
