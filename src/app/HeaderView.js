f0a3d8d49f4d037d9ee2c4a605e6748d12f7e8b0
Leonardo Aretakis <leoaretakis@gmail.com>
2014-09-10 02:01:04 +0100

    refactor page header

---
+++ src/app/HeaderView.js
@@ -0,0 +1,80 @@
+define(function(require, exports, module) {
+    var Surface         = require('famous/core/Surface');
+    var View            = require('famous/core/View');
+    var StateModifier = require('famous/modifiers/StateModifier');
+    var Transform       = require('famous/core/Transform');
+    var ImageSurface    = require('famous/surfaces/ImageSurface');
+
+    function HeaderView() {
+        View.apply(this, arguments);
+
+        _createBackground.call(this);
+        _createCloseButton.call(this);
+        _createTitle.call(this);
+        _createNextButton.call(this);
+    }
+
+    HeaderView.prototype = Object.create(View.prototype);
+    HeaderView.prototype.constructor = HeaderView;
+
+    HeaderView.DEFAULT_OPTIONS = {};
+
+    function _createBackground(){
+        var backgroundSurface = new Surface({
+            properties: {
+                backgroundColor: 'black'
+            }
+        });
+
+        var backgroundModifier = new StateModifier({
+            transform: Transform.behind
+        });
+
+        this.add(backgroundModifier).add(backgroundSurface);
+    }
+
+    function _createCloseButton(){
+        this.closeSurface = new ImageSurface({
+            size: [44, 44],
+            content: 'img/x-mark.png'
+        });
+
+        var closeModifier = new StateModifier({
+            origin: [0, 0.5],
+            align : [0, 0.5]
+        });
+
+        this.add(closeModifier).add(this.closeSurface);
+    }
+
+    function _createTitle(){
+        var titleSurface = new Surface({
+            size: [232, 44],
+            content: 'Select date and time'
+        });
+
+        var titleModifier = new StateModifier({
+            origin: [0.5, 0.5],
+            align : [0.5, 0.5]
+        });
+
+        this.add(titleModifier).add(titleSurface);
+    }
+
+    function _createNextButton(){
+        var nextSurface = new Surface({
+            size: [44, 44],
+            content: 'Next'
+        });
+
+        var nextModifier = new StateModifier({
+            origin: [1, 0.5],
+            align : [1, 0.5]
+        });
+
+        this.add(nextModifier).add(nextSurface);
+    }
+
+    module.exports = HeaderView;
+
+});
--- src/app/PageView.js
+++ src/app/PageView.js
@@ -5,8 +5,9 @@ define(function(require, exports, module) {
     var StateModifier = require('famous/modifiers/StateModifier');
 
     var HeaderFooter    = require('famous/views/HeaderFooterLayout');
-    var ImageSurface    = require('famous/surfaces/ImageSurface');
     // var FastClick       = require('famous/inputs/FastClick');
+
+    var HeaderView = require('./HeaderView')
 
     function PageView() {
         View.apply(this, arguments);
@@ -34,52 +35,9 @@ define(function(require, exports, module) {
         this.add(layoutModifier).add(this.layout);
     }
 
-    function _createHeader() {
-        var backgroundSurface = new Surface({
-            properties: {
-                backgroundColor: 'black'
-            }
-        });
-
-        var backgroundModifier = new StateModifier({
-            transform: Transform.behind
-        });
-
-        this.layout.header.add(backgroundModifier).add(backgroundSurface);
-
-        this.closeSurface = new ImageSurface({
-            size: [44, 44],
-            content: 'img/x-mark.png'
-        });
-
-        var titleSurface = new Surface({
-            size: [232, 44],
-            content: 'Select date and time'
-        });
-
-        var nextSurface = new Surface({
-            size: [44, 44],
-            content: 'Next'
-        });
-
-         var closeModifier = new StateModifier({
-            origin: [0, 0.5],
-            align : [0, 0.5]
-        });
-
-        var titleModifier = new StateModifier({
-            origin: [0.5, 0.5],
-            align : [0.5, 0.5]
-        });
-
-        var nextModifier = new StateModifier({
-            origin: [1, 0.5],
-            align : [1, 0.5]
-        });
-
-        this.layout.header.add(closeModifier).add(this.closeSurface);
-        this.layout.header.add(titleModifier).add(titleSurface);
-        this.layout.header.add(nextModifier).add(nextSurface);
+    function _createHeader(){
+        this.headerView = new HeaderView();
+        this.layout.header.add(this.headerView);
     }
 
     module.exports = PageView;
